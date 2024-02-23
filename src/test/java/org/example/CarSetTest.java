7d67495b279c0574ec7afb54b4598b607459aa81
alex-zim-98 <alexandr.zimarev.64@gmail.com>
2024-02-23 02:32:24 -0800

    hashSet: unitTest on a function add

--- src/test/java/org/example/CarSetTest.java
+++ src/test/java/org/example/CarSetTest.java
@@ -1,12 +1,33 @@
 package org.example;
 
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class CarSetTest {
+    CarSet carSet;
+    Car car;
 
     @Before
     public void setUp() throws Exception {
+        // Init
+        car = new Car("Test", 1);
+        for (int i = 0; i < 100; i++) {
+            carSet.add(new Car("Brand"+i, i));
+        }
     }
+
+    @Test
+    public void whenAddedNewElement() {
+        assertTrue(carSet.add(car));
+        assertEquals(101, carSet.size());
+    }
+
+    @Test
+    public void whenAddedOldElement() {
+        assertFalse(carSet.add(car));
+        assertEquals(101, carSet.size());
+    }
+
 }
