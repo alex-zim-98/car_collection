b174a7dadd8a93649a2d594e887b979f49cc4211
alex-zim-98 <alexandr.zimarev.64@gmail.com>
2024-02-23 02:26:14 -0800

    hashSet: create unitTest

---
+++ src/test/java/org/example/CarSetTest.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class CarSetTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+}
