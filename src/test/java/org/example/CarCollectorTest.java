0c0d13572dc1c6b434d0f8c0d0ceadb3d1378464
alex-zim-98 <alexandr.zimarev.64@gmail.com>
2024-02-23 07:38:24 -0800

    collection: create unitTest

---
+++ src/test/java/org/example/CarCollectorTest.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class CarCollectorTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+}
