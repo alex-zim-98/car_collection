619a59a9068581b558f7e3041f86bbf45458f60c
alex-zim-98 <alexandr.zimarev.64@gmail.com>
2024-02-23 07:48:45 -0800

    collection: unitTest on a function add

--- src/test/java/org/example/CarCollectorTest.java
+++ src/test/java/org/example/CarCollectorTest.java
@@ -1,12 +1,25 @@
 package org.example;
 
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class CarCollectorTest {
+    CarCollector carCollector;
 
     @Before
     public void setUp() throws Exception {
+        // Init
+        for (int i = 0; i < 100; i++) {
+            carCollector.add(new Car("Brand" + i, i));
+        }
     }
+
+    @Test
+    public void whenAddNewElement() {
+        assertTrue(carCollector.add(new Car("Brand 100", 100)));
+        assertEquals(101, carCollector.size());
+    }
+
 }
